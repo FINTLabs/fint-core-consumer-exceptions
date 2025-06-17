40c3b912e140dcb107a33a9d3c55b2605e5464e7
Hknots <hknots99@gmail.com>
2025-06-17 09:46:01 +0200

    initial commit

---
+++ src/test/kotlin/no/fintlabs/consumer/exceptions/FintCoreConsumerExceptionsApplicationTests.kt
@@ -0,0 +1,13 @@
+package no.fintlabs.consumer.exceptions
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FintCoreConsumerExceptionsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
